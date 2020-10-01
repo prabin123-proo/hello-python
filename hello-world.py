3e72a45b88bcd823947943eddbce664dfc0f0c41
prabin123-proo <71684870+prabin123-proo@users.noreply.github.com>
2020-10-01 21:21:34 +0545

    more intresting
    if any other run your programme they it take input for their name and congratulate them for their first programme which help them to motivate to learn programming :)

--- hello-world.py
+++ hello-world.py
@@ -1,2 +1,5 @@
-name = "Josh Davis"
-print(f'Hello {name}!')
+#taking user input to congratulate user for their first programme 
+name = input("Enter your name to make your first python programme ")
+print(f'Congratulation, {name}!')
+print(f'you have successfuly created your python programme :)')
+print(f"{name}, now you are a python programmer")
